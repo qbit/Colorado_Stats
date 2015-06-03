bf75c8a34e2ec2d16deea261d22e142061875ee6
Aaron Bieber <aaron@bolddaemon.com>
2015-06-03 10:25:44 -0600

    get average for all cities

---
+++ average_all.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+BIFS=$IFS
+
+IFS='
+'
+
+for c in $(cat 2013_murders_per_100k.md  | awk -F\| '{print $3}' | grep -v "^ City" | grep -v ":"); do 
+	./average_city.sh "$c"
+done
+
+IFS=$BIFS
