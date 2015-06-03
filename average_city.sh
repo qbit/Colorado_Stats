b650c2840a1ccf94efa9f3c254df3c782c303f46
Aaron Bieber <aaron@bolddaemon.com>
2015-06-03 10:25:31 -0600

    quote

--- average_city.sh
+++ average_city.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo -n "$1 : "
-grep $1 *.md | awk -F\| '{count+=1; v+=$2;} END { print v/count }' 
+AVG=$(grep "$1" *.md | awk -F\| '{count+=1; v+=$2;} END { print v/count }')
+echo "${AVG}: '$1'"
